9c99175158041db976f8808a14e0b9ea9670754a
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-01-14 11:07:54 +0500

    astrum-market

---
+++ brands/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'brands'
+
+urlpatterns = [
+    path('brand-create/', views.create_brand, name='create')
+]
---
+++ catalogs/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from products.base_models import BaseModel
+
+
+class Category(BaseModel):
+    pass
